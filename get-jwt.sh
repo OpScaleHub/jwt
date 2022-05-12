89d3bdf31733029487dcbd000794efc6fc11e1ee
AliRezaTaleghani <shayne.alone@gmail.com>
2022-05-12 21:00:25 +0200

    adding install script

--- get-jwt.sh
+++ get-jwt.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-RELEASE_TAG=$(http https://api.github.com/repos/OpScaleHub/jwt/releases/latest | jq -r '.name')
-wget -qO- https://github.com/OpScaleHub/jwt/releases/download/${RELEASE_TAG}/jwt.tgz | tar -xvz
+curl -L "https://github.com/OpScaleHub/jwt/releases/download/$(curl --silent https://api.github.com/repos/OpScaleHub/jwt/releases/latest | jq -r '.name')/jwt.tgz" | tar -xvz
 install jwt /usr/local/bin/jwt
 rm -f jwt
