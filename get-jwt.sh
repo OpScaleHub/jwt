7efc8a11dd3ccdfe5a7e81d8bcbe3be9cc4d65b8
AliReza Taleghani <shayne.alone@gmail.com>
2022-12-06 21:08:02 +0100

    update installer script

--- get-jwt.sh
+++ get-jwt.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
-curl -L "https://github.com/OpScaleHub/jwt/releases/download/$(curl --silent https://api.github.com/repos/OpScaleHub/jwt/releases/latest | jq -r '.name')/jwt"
+########################################
+### installer script [Gnu/Linux]     ###
+########################################
+
+```bash
+curl --output jwt -L "https://github.com/OpScaleHub/jwt/releases/download/$(curl --silent https://api.github.com/repos/OpScaleHub/jwt/releases/latest | jq -r '.name')/jwt"
 install jwt /usr/local/bin/jwt
 rm -f jwt
+```
